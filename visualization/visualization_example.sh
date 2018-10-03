8e14f659bd7799a504809320775957bf2bc7762e
David Rau <lgpu0088@test3.irc.sara.nl.irc.sara.nl>
2018-10-04 01:00:29 +0200

    add acc_dict.p, new image, update visualization_example

--- visualization/visualization_example.sh
+++ visualization/visualization_example.sh
@@ -1,1 +1,1 @@
-python3 visualization.py --train '../rnn/data/train.txt' --legal_chars '../rnn/gru_padded/models/legal_chars.p' --acc_dict '../rnn/gru_padded/models/acc_dict.p' --save_path 'figure.png'
+python3 visualization.py --train '../rnn/data/train.txt' --legal_chars '../rnn/data/legal_chars.p' --acc_dict '../rnn/models/acc_dict.p' --save_fig 'figure_rnn.png'
